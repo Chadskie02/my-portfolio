9900339c71b9a17670662adbb99dba173db5edf3
Richard Santos <72532825+Chadskie02@users.noreply.github.com>
2024-02-24 02:18:30 +0800

    revert to old design

--- js/nav.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('mobile-menu').addEventListener('click', function() {
-    document.querySelector('.navbar').classList.toggle('active');
-    this.classList.toggle('active');
-});
